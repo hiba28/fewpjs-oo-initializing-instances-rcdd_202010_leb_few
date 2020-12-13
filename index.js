4f0b95ce14a739312ea2957bed2eeafcb1f1a8c5
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-13 22:06:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here
+class Dog{
+  constructor(name, breed){
+    this.name = name ;
+    this.breed = breed;
+  }
+}
+let fluffyDog1 = new Dog("max","greatPyrenees")
+let fluffyDog2 = new Dog("buzz","labrador")
