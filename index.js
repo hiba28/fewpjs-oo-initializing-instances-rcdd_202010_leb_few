1f459fcc9a4637b51727627630dc575b7e315160
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-13 22:18:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class Dinner{
     this._dessert = dessert
   }
   listDinner(){
-    this._dessert = "cake"
+    this._dessert = ""
     return `${this.salad} + ${this.soup} + ${this.entree} + ${this._dessert}`
   }
 }
